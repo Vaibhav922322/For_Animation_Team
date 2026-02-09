dd54d79b8784146630fc053c881af4b2968ca0cc
vaibhav <vaibhav@naitri.ai>
2026-02-09 17:21:12 +0530

    Added CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,19 +1,25 @@
 from fastapi import FastAPI
 from apis.routes import router as api_router
 from smb_client.explorer import SBMFileExplorer
+from fastapi.middleware.cors import CORSMiddleware
+
 def create_app() -> FastAPI:
     app = FastAPI(title="My API", version="1.0.0")
 
     # add all routes from routes/ in one shot
     app.include_router(api_router)
-
-    # @app.on_event("startup")
-    # async def startup_event():
-    #     # Start the file scan synchronously to block server startup until complete
-    #     print("Starting synchronous file scan. The server will not accept requests until this finishes...")
-    #     SBMFileExplorer.get_all_files()
-    #     print("File scan completed. Server is now ready.")
-    # return app
+    # Start the file scan synchronously to block server startup until complete
+    print("Starting synchronous file scan. The server will not accept requests until this finishes...")
+    SBMFileExplorer.get_all_files()
+    print("File scan completed. Server is now ready.")
     return app
 
 app = create_app()
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],  # Allows all origins
+        allow_credentials=True,
+        allow_methods=["*"],  # Allows all methods
+        allow_headers=["*"],  # Allows all headers
+    )
